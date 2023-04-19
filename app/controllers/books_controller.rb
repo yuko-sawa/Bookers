f14c58501f6adee44a0b50ab91f8c72618afe23f
YukoSawai <129607371 +yuko-sawa@users.noreply.github.com>
2023-04-19 16:34:44 +0000

    books rayout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+ 
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] ="Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -20,10 +23,13 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    end
   end
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
