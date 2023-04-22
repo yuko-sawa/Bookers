9478511c2e460fd25fe61dab576ad60d6f3fd739
YukoSawai <129607371 +yuko-sawa@users.noreply.github.com>
2023-04-22 02:31:31 +0000

    yet test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,8 +34,8 @@ class BooksController < ApplicationController
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to '/books'
   end
   
--- spec/system/books_spec.rb
+++ spec/system/books_spec.rb
@@ -8,7 +8,7 @@ describe '投稿のテスト' do
     end
     context '表示の確認' do
       it 'トップ画面(root_path)に一覧ページへのリンクが表示されているか' do
-        expect(page).to have_link "", href: books_path
+        expect(page).to have_link "start", href: books_path
       end
       it 'root_pathが"/"であるか' do
         expect(current_path).to eq('/')
